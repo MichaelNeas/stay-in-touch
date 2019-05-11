c43f3589b28216345a1ccec3e2fb8f2ebf206143
michaelneas <michael.r.neas@gmail.com>
2019-05-11 00:31:36 -0400

    removes table view from testing contacts retrieval

--- StayInTouch/Controllers/ContactsViewController.swift
+++ StayInTouch/Controllers/ContactsViewController.swift
@@ -13,67 +13,33 @@ class ContactsViewController: UIViewController {
     
     var phoneContacts = [PhoneContact]()
     var filter: ContactsFilter = .none
-
-    @IBOutlet weak var tableView: UITableView!
+    let contactViewController = ContactMethodViewController()
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var letsGoButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loadContacts(filter: filter)
-        let contactViewController = ContactMethodViewController()
+        nameLabel.isHidden = true
+        
         addChild(contactViewController)
+        contactViewController.view.isHidden = true
         view.addSubview(contactViewController.view)
         contactViewController.view.frame = CGRect(x: 0, y: view.center.y, width: view.bounds.width, height: view.bounds.height / 2)
         contactViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         contactViewController.didMove(toParent: self)
     }
     
-    func phoneNumberWithContryCode() -> [String] {
-        let contacts = PhoneContacts.getContacts()
-        var arrPhoneNumbers = [String]()
-        for contact in contacts {
-            for ContctNumVar: CNLabeledValue in contact.phoneNumbers {
-                let fulMobNumVar = ContctNumVar.value
-                //let countryCode = fulMobNumVar.value(forKey: "countryCode") get country code
-                if let MccNamVar = fulMobNumVar.value(forKey: "digits") as? String {
-                    arrPhoneNumbers.append(MccNamVar)
-                }
-            }
-        }
-        return arrPhoneNumbers
+    @IBAction func letsGoButtonAction(_ sender: Any) {
+        contactViewController.view.isHidden = false
+        nameLabel.text = phoneContacts.randomElement()?.name
+        nameLabel.isHidden = false
     }
     
     fileprivate func loadContacts(filter: ContactsFilter) {
         phoneContacts.removeAll()
-        var allContacts = [PhoneContact]()
         for contact in PhoneContacts.getContacts(filter: filter) {
-            allContacts.append(PhoneContact(contact: contact))
+            phoneContacts.append(PhoneContact(contact: contact))
         }
-        
-        var filterdArray = [PhoneContact]()
-        if self.filter == .mail {
-            filterdArray = allContacts.filter({ $0.email.count > 0 }) // getting all email
-        } else if self.filter == .message {
-            filterdArray = allContacts.filter({ $0.phoneNumber.count > 0 })
-        } else {
-            filterdArray = allContacts
-        }
-        phoneContacts.append(contentsOf: filterdArray)
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-        }
-    }
-}
-
-
-extension ContactsViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return phoneContacts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
-        let text = phoneContacts[indexPath.row].name
-        cell.textLabel?.text = text
-        return cell
     }
 }

eac488977f6233846a87c0a66aa9ea5cfd8d64f0
michaelneas <michael.r.neas@gmail.com>
2019-07-17 08:57:01 -0400

    adds instabug and cocoapods

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'StayInTouch' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for StayInTouch
+  pod 'Instabug' 
+
+  target 'StayInTouchTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'StayInTouchUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- StayInTouch/App/AppDelegate.swift
+++ StayInTouch/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Instabug
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Instabug.start(withToken: "62c0c56928e7204e814a9d41a237813e", invocationEvents: [.shake, .screenshot])
         return true
     }
 

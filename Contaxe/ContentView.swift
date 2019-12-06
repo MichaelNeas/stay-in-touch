ce1d8f504b47cc65070343ef1b25ad4751d8d641
michaelneas <michael.r.neas@gmail.com>
2019-12-05 22:03:46 -0500

    prototype swift ui with demo data

--- Contaxe/ContentView.swift
+++ Contaxe/ContentView.swift
@@ -9,38 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    struct ContactButton: View {
+        var systemImage: String
+        
+        var body: some View {
+            Button(action: {
+                print("tapped")
+            }) {
+                Image(systemName: systemImage)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding(36.0)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Circle()
-                .fill(Color.green)
+            Spacer(minLength: 100.0)
+            
+            Text("DH")
+                .font(Font.system(size: 60, design: .default))
+                .fontWeight(.bold)
+                .padding(80.0)
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .padding(.bottom, 40.0)
+
             
             VStack {
                 Text("Dana Higgins")
                     .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
                 Text("Boston, MA")
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
             }.padding()
-            
             
             VStack {
                 HStack {
-//                    Button(action: {
-//                        self.tapped(1)
-//                    }) {
-//                        Image().renderingMode(.original)
-//                            .clipShape(Circle())
-//                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-//                            .shadow(color: .black, radius: 2)
-//                    }.padding()
-                    Circle()
-                }
+                    ContactButton(systemImage: "message")
+                    ContactButton(systemImage: "phone")
+                }.padding()
                 HStack {
-                    Circle()
-                    Circle()
-                }
-            }
+                    ContactButton(systemImage: "envelope")
+                    ContactButton(systemImage: "video")
+                }.padding()
+            }.padding(EdgeInsets(top: 10.0, leading: 36.0, bottom: 60.0, trailing: 36.0))
             
-        }.padding()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: [.darkPurple, .cyan]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .edgesIgnoringSafeArea(.all)
     }
     
     func tapped(_ number: Int) {
---
+++ Contaxe/Extensions/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Contaxe
+//
+//  Created by Michael Neas on 12/4/19.
+//  Copyright © 2019 Neas Lease. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    public static let cyan = Color(red: 57.0/255.0, green: 247.0/255.0, blue: 219.0/255.0)
+    public static let darkPurple = Color(red: 117.0/255.0, green: 40.0/255.0, blue: 221.0/255.0)
+}
